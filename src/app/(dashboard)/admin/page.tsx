d030487cc70b40d93980a0812b91d0d8834fe1ee
Will D <diweizhi@gmail.com>
2025-07-14 14:21:21 +1000

    Line Chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from '@/components/AttendanceChart';
 import CountChart from '@/components/CountChart';
+import FinanceChart from '@/components/FinanceChart';
 import UserCard from '@/components/UserCard';
 
 const AdminPage = () => {
@@ -25,8 +26,10 @@ const AdminPage = () => {
             <AttendanceChart />
           </div>
         </div>
-        {/* BOTTOM CHARTS */}
-        <div></div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3">r</div>

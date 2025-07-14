3681eb896c8993cde6450707b10d8e9e14d52b5f
Will D <diweizhi@gmail.com>
2025-07-14 10:20:30 +1000

    Admin Dashborad Design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,22 @@
+import UserCard from '@/components/UserCard';
+
 const AdminPage = () => {
-  return <div className="">AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="students" />
+          <UserCard type="teachers" />
+          <UserCard type="parents" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
 };
 
 export default AdminPage;

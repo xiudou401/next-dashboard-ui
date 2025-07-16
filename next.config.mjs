e57a5330e9cfe69ba41d0ff1b0483731b1ff2054
Will D <diweizhi@gmail.com>
2025-07-16 10:04:58 +1000

    Teacher List

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+import { hostname } from 'os';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: false,
+  images: {
+    remotePatterns: [{ hostname: 'images.pexels.com' }],
+  },
 };
 
 export default nextConfig;
